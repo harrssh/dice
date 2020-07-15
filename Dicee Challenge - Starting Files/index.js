594f942c69f21ba4dd5d87447e619e3b0925aa9c
harrssh <65028945+harrssh@users.noreply.github.com>
2020-07-15 19:53:59 +0530

    Add files via upload

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,24 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; // it will give random number between 1 to 6
+var randomDiaceImage = "dice" + randomNumber1 + ".png"; //it will create random inage name between 1 t0 6
+var randomImageSource = "images/" + randomDiaceImage;
+var imageSelector = document.querySelectorAll("img")[0];
+imageSelector.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; // it will give random number between 1 to 6
+var randomDiaceImage2 = "dice" + randomNumber2 + ".png"; //it will create random inage name between 1 t0 6
+var randomImageSource2 = "images/" + randomDiaceImage2;
+var imageSelector2 = document.querySelectorAll("img")[1];
+imageSelector2.setAttribute("src", randomImageSource2);
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 1 Won !";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Won !";
+}else{
+  document.querySelector("h1").innerHTML = "Game Draw !";
+}
+
+function refreshPage(){
+    window.location.reload();
+} 
